99e172f2282929d9bd68138bfdcfde8c1fbbd003
Jorge Barragan <jmarq19@outlook.com>
2021-12-07 14:59:03 -0800

    adding my first two front end mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,37 @@
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+    _id
+    username
+    email
+    password
+    savedBooks {
+      _id
+      bookID
+      title
+      description
+      image
+      link
+        } 
+      }
+    }
+  }`;
+
+export const ADD_USER = gql`
+mutation addUser($email: String!, $username: String!, $password String!) {
+  addUser(email: $email, username: $username, password: $password) {
+    token
+    user {
+      _id
+      first_name
+      last_name
+      username
+      email
+      password
+      profpic
+      aboutme
+    }
+  }
+}`;
